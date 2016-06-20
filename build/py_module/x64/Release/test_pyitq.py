a90d1c57f6eb1f26f44f083477d7e8f2a4ee624c
primetang <tanggefu@gmail.com>
2016-06-20 17:40:27 +0800

    format some python code

--- build/py_module/x64/Release/test_pyitq.py
+++ build/py_module/x64/Release/test_pyitq.py
@@ -6,16 +6,21 @@ import pyitq
 queryset = loadmat('queryset.mat')['queryset']
 itq = pyitq.itq_f()
 itq.save_hash(
-	'E:/GitHub/File-Based-ITQ/test',
-	'E:/GitHub/File-Based-ITQ/test',
-	20,
-	3,
-	6,
-	5000,
-	200
+    'E:/GitHub/File-Based-ITQ/test',
+    'E:/GitHub/File-Based-ITQ/test',
+    20,
+    3,
+    6,
+    5000,
+    200
 )
-itq.load_hash('E:/GitHub/File-Based-ITQ/test',
-              'E:/GitHub/File-Based-ITQ/test/ITQ_L-3_N-6_S-5000_I-200', 2, 30, 4096)
+itq.load_hash(
+    'E:/GitHub/File-Based-ITQ/test',
+    'E:/GitHub/File-Based-ITQ/test/ITQ_L-3_N-6_S-5000_I-200',
+    2,
+    30,
+    4096
+)
 for each in queryset:
     print '----------------------------------------------'
     res = itq.query(each.tolist(), 5, 0)
